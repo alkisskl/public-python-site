f239bfddede2c7b7f5f98d75986c26906e21ab6f
Alkis Sklavounakis <skla@sitecore.net>
2023-01-03 17:08:34 +0200

    * create a hello world python website

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import sys
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/hostname")
+def hostname():
+    hostname = os.system("hostname")
+    return hostname
